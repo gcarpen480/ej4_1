8a6957e6562a6d3b4bf5de6cb2002dcc78978e3b
gcarpen480 <gcarpen480@g.educaand.es>
2025-02-28 11:52:00 +0100

    Subiendo Adapter realizados

--- src/main/java/ies/castillodeluna/ad/backend/adapter/PedidoAdapter.java
+++ src/main/java/ies/castillodeluna/ad/backend/adapter/PedidoAdapter.java
@@ -1,5 +1,120 @@
 package ies.castillodeluna.ad.backend.adapter;
 
-public class PedidoAdapter {
-    
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import edu.acceso.sqlutils.dao.Crud;
+import edu.acceso.sqlutils.errors.DataAccessException;
+import ies.castillodeluna.ad.backend.hibernate.HibernateUtil;
+import ies.castillodeluna.ad.models.Pedido;
+
+public class PedidoAdapter implements Crud<Pedido> {
+
+    @Override
+    public boolean delete(int id) throws DataAccessException {
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Pedido pedido = session.find(Pedido.class, id);
+
+            if (pedido != null) {
+                session.remove(pedido);
+                transaction.commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataAccessException(e);
+        }
+    }
+
+    @Override
+    public Stream<Pedido> get() throws DataAccessException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();            
+            List<Pedido> pedidos = session.createQuery("FROM Pedido", Pedido.class).list();
+            return pedidos.stream();
+        } catch (Exception e) {
+            throw new DataAccessException(e);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<Pedido> get(int id) throws DataAccessException {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Pedido pedido = session.find(Pedido.class, id);
+            return Optional.ofNullable(pedido);
+        } catch (Exception e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    @Override
+    public void insert(Pedido pedido) throws DataAccessException {
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(pedido);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataAccessException(e);
+        }
+    }
+
+    @Override
+    public boolean update(Pedido pedido) throws DataAccessException {
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.merge(pedido);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataAccessException(e);
+        }
+    }
+
+    @Override
+    public boolean update(int oldId, int newId) throws DataAccessException {
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Pedido pedido = session.find(Pedido.class, oldId);
+
+            if (pedido != null) {
+                pedido.setId(newId);
+                session.merge(pedido);
+                transaction.commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataAccessException(e);
+        }
+    }
 }
--- src/main/java/ies/castillodeluna/ad/backend/adapter/ZonaEnvioAdapter.java
+++ src/main/java/ies/castillodeluna/ad/backend/adapter/ZonaEnvioAdapter.java
@@ -1,5 +1,121 @@
 package ies.castillodeluna.ad.backend.adapter;
 
-public class ZonaEnvioAdapter {
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import edu.acceso.sqlutils.dao.Crud;
+import edu.acceso.sqlutils.errors.DataAccessException;
+import ies.castillodeluna.ad.backend.hibernate.HibernateUtil;
+import ies.castillodeluna.ad.models.ZonaEnvio;
+
+public class ZonaEnvioAdapter implements Crud<ZonaEnvio> {
     
+    @Override
+    public boolean delete(int id) throws DataAccessException {
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            ZonaEnvio zonaEnvio = session.find(ZonaEnvio.class, id);
+
+            if (zonaEnvio != null) {
+                session.remove(zonaEnvio);
+                transaction.commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataAccessException(e);
+        }
+    }
+
+    @Override
+    public Stream<ZonaEnvio> get() throws DataAccessException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            List<ZonaEnvio> zonas = session.createQuery("FROM ZonaEnvio", ZonaEnvio.class).list();
+            return zonas.stream();
+        } catch (Exception e) {
+            throw new DataAccessException(e);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<ZonaEnvio> get(int id) throws DataAccessException {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            ZonaEnvio zonaEnvio = session.find(ZonaEnvio.class, id);
+            return Optional.ofNullable(zonaEnvio);
+        } catch (Exception e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    @Override
+    public void insert(ZonaEnvio zonaEnvio) throws DataAccessException {
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(zonaEnvio);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataAccessException(e);
+        }
+    }
+
+    @Override
+    public boolean update(ZonaEnvio zonaEnvio) throws DataAccessException {
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.merge(zonaEnvio);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataAccessException(e);
+        }
+    }
+
+    @Override
+    public boolean update(int oldId, int newId) throws DataAccessException {
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            ZonaEnvio zonaEnvio = session.find(ZonaEnvio.class, oldId);
+
+            if (zonaEnvio != null) {
+                zonaEnvio.setId(newId);
+                session.merge(zonaEnvio);
+                transaction.commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataAccessException(e);
+        }
+    }
+
 }
